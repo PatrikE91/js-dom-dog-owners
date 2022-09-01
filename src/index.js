32db17f66e04ea4bf915c6ba058d6cc9b7f989af
patrik <patrik.ellini@outlook.com>
2022-09-01 17:51:16 +0100

    base structure incomplete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,158 @@
-console.log(data);
+function createDogListItem(dog) {
+  const li = document.createElement("li");
+  const dogContainer = document.querySelector(".main");
 
-// WRITE YOUR CODE BELOW!
+  li.className = "dogs-list__button";
+  li.innerText = dog.name;
+  dogContainer.append(li)
+  
+  li.addEventListener("click", () => {
+    const mainDogSection = document.querySelector(".main__dog-section");
+    mainDogSection.innerHTML = '';
+    createDogCard(dog);
+  });
+  return li;
+}
+
+function createSection() {
+  const section = document.createElement("section");
+  section.className = "main__dog-section";
+  return section;
+}
+
+const createDogCardDesc = (bio) => {
+  const div = document.createElement("div");
+  div.className = "main__dog-section__desc";
+
+  //(2)
+  //TODO: Update this section to create new elements
+  //for the bio header and the bio text. Add those
+  //elements to the div.
+  const header = document.createElement("h3");
+  const text = document.createElement("p");
+  div.append(header, text);
+
+  return div;
+};
+
+function createDogCardBottomSection(dog) {
+  const button = document.createElement("button");
+  const text = document.createElement("p");
+  const div = document.createElement("div");
+
+  div.className = "main__dog-section__btn";
+
+  let dogBehave = dog.isGoodDog === true ? "Good Dog" : "Bad Dog!";
+  button.innerText = dogBehave
+
+  div.append(text, button);
+  return div;
+}
+
+const createDogCard = (dog) => {
+  const section = createSection();
+  const header = document.createElement("h2");
+  header.innerText = dog.name;
+
+  const img = document.createElement("img");
+  img.setAttribute("src", dog.image);
+
+  const desc = createDogCardDesc(dog.bio);
+  const bottomSection = createDogCardBottomSection(dog);
+
+  section.append(header, desc, bottomSection);
+
+  return section;
+};
+
+function createForm() {
+  const form = document.createElement("form");
+
+  const nameInput = createInput("name");
+  const imgInput = createInput("image", "url");
+  const bioInput = createInput("bio", "textarea");
+  const submitInput = createInput("submit", "submit", "Let's add a dog!");
+
+  const nameLabel = createLabel("name", "Dog's name");
+  const imgLabel = createLabel("image", "Dog's picture");
+  const bioLabel = createLabel("bio", "Dog's bio");
+
+  form.className = "form";
+  submitInput.className = "form__button";
+
+  //(5)
+  //TODO: Add an event listener on to the form to capture the
+  //submit event. In the submit event, add a item  to the
+  //list of dogs at the top of the page, and add a new object
+  //in to the dogs array with the data captured from the form.
+
+  form.append(
+    nameLabel,
+    nameInput,
+    imgLabel,
+    imgInput,
+    bioLabel,
+    bioInput,
+    submitInput
+  );
+  return form;
+}
+
+function createInput(idName, type = "text", value) {
+  let input = null;
+
+  if (type === "textarea") {
+    input = document.createElement("textarea");
+    input.setAttribute("rows", "5");
+  } else {
+    input = document.createElement("input");
+    input.setAttribute("type", type);
+  }
+
+  input.setAttribute("id", idName);
+  input.setAttribute("name", idName);
+
+  if (value) input.setAttribute("value", value);
+
+  return input;
+}
+
+function createLabel(forAttr, text) {
+  const label = document.createElement("label");
+  label.attributes.for = forAttr;
+  label.innerText = text;
+
+  return label;
+}
+
+function renderMainForm() {
+  const section = createSection();
+  const form = createForm();
+  const h2 = document.createElement("h2");
+
+  h2.innerText = "Add a new Dog";
+
+  section.append(h2, form);
+
+  return section;
+}
+
+function renderDogList(dogsArr) {
+  const listContainer = document.querySelector(".dogs-list");
+  for (const dog of dogsArr) {
+    const item = createDogListItem(dog);
+    listContainer.append(item);
+  }
+}
+
+renderDogList(data);
+
+const formButton = document.querySelector(".dogs-list__button--add");
+const dogContainer = document.querySelector(".main");
+
+// (4)
+//TODO: Add an event listener on to form button so that
+//when the user clicks the button, the form is displayed.
+//the renderMainForm method will return an element that
+//contains the form, so you only need to add it to the
+//dogContainer and remove any existing children.
